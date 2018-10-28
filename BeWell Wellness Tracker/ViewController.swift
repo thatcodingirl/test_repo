387739e7d528542ec8ea3a3c5fed434256d995e3
jesicaqp <jesicaquiper@gmail.com>
2018-10-28 13:45:15 -0400

    Initial Commit

---
+++ BeWell Wellness Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeWell Wellness Tracker
+//
+//  Created by Jesica Quinones on 10/28/18.
+//  Copyright © 2018 Jesica Quinones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
